7a9239a394930f770e2059e863d241ac7776b1e4
Zeyad Zahran <20912023100721@fci.zu.edu.eg>
2025-09-03 11:25:04 +0300

    add double jump as a power up and add enemy base class

---
+++ Assets/Scripts/doubleJump.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class DoubleJump : MonoBehaviour
+{
+    public int heartValue = 1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerMovement player = other.GetComponent<PlayerMovement>();
+            if (player != null)
+            {
+                player.AddHeart(heartValue);
+                Debug.Log("Collected a Heart ");
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
