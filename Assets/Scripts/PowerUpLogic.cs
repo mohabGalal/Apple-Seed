b9e8b052e824c7c5481d8a25ce864575bf438cac
Zeyad Zahran <20912023100721@fci.zu.edu.eg>
2025-09-03 03:48:08 +0300

    powerUps Logic

--- Assets/Scripts/PowerUpLogic.cs
+++ Assets/Scripts/PowerUpLogic.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PowerUpLogic : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
-    {
+    { 
         if (other.CompareTag("Player"))
         {
             Debug.Log("Hit PowerUp");
--- Assets/Scripts/PowerUps.cs
+++ Assets/Scripts/PowerUps.cs
@@ -4,47 +4,79 @@ using UnityEngine;
 
 public class PowerUps : MonoBehaviour
 {
-  //  public Dictionary<string, GameObject> powerUps;
+    //  public Dictionary<string, GameObject> powerUps;
     public List<GameObject> powerUpPrefab; //Assign the PowerUp assets (prefabs) from the inspector
     public Transform PowerUpSpawner;
     public Transform StartingPoint;
     public Transform EndingPoint;
 
-    Vector3 newPos = Vector3.zero;
+    public int numberOfPowerUps = 10 ;
     float MinDistance = 6f;
-    
+    private List<Vector3> spawnedPositions = new List<Vector3>();
+
 
 
 
     void Start()
     {
-       // powerUps = new Dictionary<string, GameObject>();
+        // powerUps = new Dictionary<string, GameObject>();
 
         //just checkking
-        for (int i = 0 ; i < powerUpPrefab.Count; i++)
+        for (int i = 0; i < powerUpPrefab.Count; i++)
         {
             Debug.Log(powerUpPrefab[i].name);
         }
 
 
-        Randomize();  // run the code multiple times you will see the pickups change randomly at the same position 
+        SpawnPowerUps();  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void Randomize()
-    {
-        float RandX = Random.Range(StartingPoint.position.x, EndingPoint.position.x);
-        float RandY = Random.Range(StartingPoint.position.y, EndingPoint.position.y);
-        newPos.x = RandX;
-        newPos.y = RandY;
-        
+   
 
-        int index = Random.Range(0, powerUpPrefab.Count);
-        GameObject CurrentPowerUp = Instantiate(powerUpPrefab[index], newPos, Quaternion.identity); 
+    private void SpawnPowerUps()
+    {
+
+        int attempts = 0;
+        int maxAttempts = numberOfPowerUps * 10;
+
+        while (spawnedPositions.Count < numberOfPowerUps && attempts < maxAttempts)
+        {
+            attempts++;
+
+
+            float RandX = Random.Range(StartingPoint.position.x, EndingPoint.position.x);
+            float RandY = Random.Range(StartingPoint.position.y, EndingPoint.position.y);
+
+            Vector3 newPos = new Vector3(RandX, RandY, 0f);
+
+
+            bool isClose = false;
+
+            foreach (var pos in spawnedPositions)
+            {
+                if (Vector3.Distance(newPos, pos) < MinDistance)
+                {
+                    isClose = true;
+                    break;
+                }
+            }
+            if (!isClose)
+            {
+                // this pos  is valied so we can add it 
+                spawnedPositions.Add(newPos);
+
+                // pick a random prefab
+
+                int idx = Random.Range(0, powerUpPrefab.Count);
+                Instantiate(powerUpPrefab[idx], newPos, Quaternion.identity);
+            }
+
+        }
     }
 }
